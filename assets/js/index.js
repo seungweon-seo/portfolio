63462214246c486bf9c6c2ed4f22b07c2e51c87e
methejohn <seoseungweon@gmail.com>
2024-11-21 18:22:59 +0900

    #1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,124 +1,114 @@
 const lenis = new Lenis();
 
-lenis.on("scroll", ScrollTrigger.update);
+// DOM
+const linkLogo = document.querySelector('.header .link-logo');
+const logoName = document.querySelector('.header .name2');
+const btnMenu = document.querySelector('.header .btn-menu');
+const groupMobile = document.querySelector('.header .group-mobile');
 
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000);
-});
+const scrollToLinks = [];
+const linkAbout = document.querySelectorAll('.header .link-about');
+const linkContact = document.querySelectorAll('.header .link-contact');
+scrollToLinks.push(...linkAbout, ...linkContact);
 
-gsap.ticker.lagSmoothing(0);
-
-$(window).on("load", function () {
-  $("body").addClass("loaded");
-  sizeChk();
-});
-
-$(".header .group-top .logo a .name-wrap .name2").on(
-  "animationend",
-  function () {
-    $(".header .group-top .logo a").hover(
-      function () {
-        $(this).addClass("on");
-      },
-      function () {
-        $(this).removeClass("on");
-      }
-    );
-  }
+const stickyProjects = document.querySelectorAll(
+  '.sc-project .sticky-project > div:not(.p6)'
 );
 
-$(".header .group-top .btn-menu").click(function () {
-  $(".header .group-mobile").toggleClass("on");
-  $("body").toggleClass("hidden");
-  $(".header .group-top .btn-menu").toggleClass("on");
-  if ($("body").hasClass("hidden")) {
-    lenis.stop();
-  } else {
+const scProject = document.querySelector('.sc-project');
+const groupProject = document.querySelector('.sc-project .group-project');
+const projectCursor = document.querySelector('.sc-project .project-cursor');
+
+// Lenis
+function lenisStart() {
+  lenis.on('scroll', ScrollTrigger.update);
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+  gsap.ticker.lagSmoothing(0);
+}
+
+lenisStart();
+
+// Load
+window.addEventListener('load', () => {
+  document.body.classList.add('loaded');
+  handleResize();
+});
+
+// Mouseover
+logoName.addEventListener('animationend', () => {
+  linkLogo.addEventListener('mouseover', () => {
+    linkLogo.classList.add('on');
+  });
+  linkLogo.addEventListener('mouseout', () => {
+    linkLogo.classList.remove('on');
+  });
+});
+
+// Click menu
+btnMenu.addEventListener('click', handleClick);
+
+function handleClick() {
+  groupMobile.classList.toggle('on');
+  document.body.classList.toggle('hidden');
+  btnMenu.classList.toggle('on');
+  document.body.classList.contains('hidden') ? lenis.stop() : lenis.start();
+}
+
+// ScrollTo
+scrollToLinks.forEach((link) => {
+  link.addEventListener('click', (e) => {
+    const id = link.classList.contains('link-about') ? '#about' : '#footer';
+    const size = window.innerWidth < 768 ? 'mobile' : 'desktop';
+    handleScrollTo(e, id, size);
+  });
+});
+
+function handleScrollTo(e, id, size) {
+  e.preventDefault();
+  if (size === 'mobile') {
+    groupMobile.classList.remove('on');
+    document.body.classList.remove('hidden');
+    btnMenu.classList.remove('on');
     lenis.start();
   }
-});
-
-$(".gnb .gnb-list .gnb-item .link-about").click(function (e) {
-  e.preventDefault();
   gsap.to(window, {
     duration: 2,
-    scrollTo: { y: "#about" },
-    ease: "power3.out",
+    scrollTo: { y: id },
+    ease: 'power3.out',
   });
-});
+}
 
-$(".gnb .gnb-list .gnb-item .link-contact").click(function (e) {
-  e.preventDefault();
-  gsap.to(window, {
-    duration: 2,
-    scrollTo: { y: "#footer" },
-    ease: "power3.out",
-  });
-});
-
-$(".header .group-mobile .gnb-list .gnb-item .link-about").click(function (e) {
-  e.preventDefault();
-  $(".header .group-mobile").removeClass("on");
-  $("body").removeClass("hidden");
-  $(".header .group-top .btn-menu").removeClass("on");
-  lenis.start();
-  gsap.to(window, {
-    duration: 2,
-    scrollTo: { y: "#about" },
-    ease: "power3.out",
-  });
-});
-
-$(".header .group-mobile .gnb-list .gnb-item .link-contact").click(function (
-  e
-) {
-  e.preventDefault();
-  $(".header .group-mobile").removeClass("on");
-  $("body").removeClass("hidden");
-  $(".header .group-top .btn-menu").removeClass("on");
-  lenis.start();
-  gsap.to(window, {
-    duration: 2,
-    scrollTo: { y: "#footer" },
-    ease: "power3.out",
-  });
-});
-
-projectTl = gsap.timeline({
+// Project timeline
+const projectTl = gsap.timeline({
   scrollTrigger: {
-    trigger: ".group-project",
-    start: "0% 0%",
-    end: "100% 100%",
+    trigger: '.group-project',
+    start: '0% 0%',
+    end: '100% 100%',
     scrub: 0,
   },
-  ease: "none",
+  ease: 'none',
 });
 
-projectTl
-  .to(".sc-project .group-intro .sticky-intro", {
-    bottom: "100%",
-  })
-  .to(".sc-project .group-project .sticky-project .p1", {
-    bottom: "100%",
-  })
-  .to(".sc-project .group-project .sticky-project .p2", {
-    bottom: "100%",
-  })
-  .to(".sc-project .group-project .sticky-project .p3", {
-    bottom: "100%",
-  })
-  .to(".sc-project .group-project .sticky-project .p4", {
-    bottom: "100%",
-  })
-  .to(".sc-project .group-project .sticky-project .p5", {
-    bottom: "100%",
-  });
+projectTl.to('.sc-project .group-intro .sticky-intro', {
+  bottom: '100%',
+});
 
-$(".sc-project").mousemove(function (e) {
-  const projectCursor = $('.project-cursor [data-target="cursor"]');
-  gsap.to(projectCursor, {
-    x: e.clientX + "px",
-    y: e.clientY + "px",
+stickyProjects.forEach((el) => {
+  projectTl.to(el, {
+    bottom: '100%',
+  });
+});
+
+// Project mousemove
+scProject.addEventListener('mousemove', (e) => {
+  const cursors = document.querySelectorAll(
+    '.project-cursor [data-target="cursor"]'
+  );
+  gsap.to(cursors, {
+    x: e.clientX + 'px',
+    y: e.clientY + 'px',
     duration: 0.7,
     stagger: {
       each: 0.1,
@@ -126,92 +116,135 @@ $(".sc-project").mousemove(function (e) {
   });
 });
 
-$(".sc-project .group-project").mouseover(function () {
-  $(".sc-project .project-cursor").addClass("on");
+// Project mouseover
+groupProject.addEventListener('mouseover', () => {
+  projectCursor.classList.add('on');
 });
 
-$(".sc-project .group-project").mouseleave(function () {
-  $(".sc-project .project-cursor").removeClass("on");
+groupProject.addEventListener('mouseleave', () => {
+  projectCursor.classList.remove('on');
 });
 
-gsap.set(".sc-about .group-title .title-area .line .word", { opacity: 0 });
+gsap.set('.sc-about .group-title .title-area .line .word', { opacity: 0 });
 
-$(window).resize(function () {
-  sizeChk();
-});
+// Init motionpath
+let tween;
+function initMotionPath() {
+  let progress = tween ? tween.progress() : 0;
+  tween && tween.progress(0).kill();
 
-function sizeChk() {
-  $(".sc-learn .group-bottom .learn-list .learn-item .name2").each(function () {
-    const name2Width = $(this).width() + 40;
-    const wrapOffset = $(this).parent(".name-wrap").offset().left;
-    const boxOffset = $(this).siblings(".name-box").offset().left;
-    const nameOffset = boxOffset - wrapOffset;
-    const minusWidth = name2Width * -1;
-
-    $(this)
-      .siblings(".name-box")
-      .css("--name2-width", name2Width + "px");
-    $(this)
-      .siblings(".name-box")
-      .css("--name-offset", nameOffset + "px");
-    $(this)
-      .siblings(".name-box")
-      .css("--minus-width", minusWidth + "px");
+  tween = gsap.to('.sphere', {
+    motionPath: {
+      path: '#orbitPath',
+      align: '#orbitPath',
+      alignOrigin: [0.5, 0.5],
+    },
+    duration: 7,
+    ease: 'none',
+    repeat: -1,
   });
-  const headerHeight = $(".header .group-top").innerHeight();
-  $(".header .group-mobile").css("--top", headerHeight + "px");
 
-  ScrollTrigger.create({
-    trigger: ".sc-about",
-    start: "0% 50%",
-    onEnter: function () {
-      gsap.to(".sc-about .group-title .title-area .line .word", {
-        opacity: 1,
-        duration: 1,
-        ease: "power1.inOut",
-        stagger: {
-          each: 0.3,
+  tween.progress(progress);
+}
+
+// Resize
+window.addEventListener('resize', () => {
+  handleResize();
+});
+
+const learnItems = document.querySelectorAll('.sc-learn .learn-item');
+
+let mobile = false;
+let mobileTriggers = null;
+const mobileBatches = document.querySelectorAll('[data-batch2]');
+
+function handleResize() {
+  // Learn offset
+  learnItems.forEach((el) => {
+    const name = el.querySelector('.name2');
+    const wrap = el.querySelector('.name-wrap');
+    const box = el.querySelector('.name-box');
+
+    const nameRect = name.getBoundingClientRect();
+    const wrapRect = wrap.getBoundingClientRect();
+    const boxRect = box.getBoundingClientRect();
+
+    const nameWidth = nameRect.width + 40;
+    const nameOffset = boxRect.left - wrapRect.left;
+    const invertedNameWidth = -nameWidth;
+
+    box.style.setProperty('--name2-width', nameWidth + 'px');
+    box.style.setProperty('--name-offset', nameOffset + 'px');
+    box.style.setProperty('--minus-width', invertedNameWidth + 'px');
+  });
+
+  // Header height
+  const headerHeight = document
+    .querySelector('.header .group-top')
+    .getBoundingClientRect().height;
+
+  groupMobile.style.setProperty('--top', headerHeight + 'px');
+
+  // Path resize
+  initMotionPath();
+
+  // Learn trigger
+  if (window.innerWidth < 768) {
+    if (!mobile) {
+      mobileTriggers = ScrollTrigger.batch(mobileBatches, {
+        start: '0% 70%',
+        end: '100% 70%',
+        onEnter: function (batches) {
+          batches.forEach((batch) => {
+            batch.classList.add('on');
+          });
+        },
+        onLeaveBack: function (batches) {
+          batches.forEach((batch) => {
+            batch.classList.remove('on');
+          });
         },
       });
-      setTimeout(() => {
-        gsap.to(".sphere", {
-          duration: 7,
-          ease: "none",
-          repeat: -1,
-          motionPath: {
-            path: "#orbitPath",
-            align: "#orbitPath",
-            alignOrigin: [0.5, 0.5],
-          },
-        });
-      }, 100);
-    },
-  });
-
-  if ($(window).width() < 768) {
-    ScrollTrigger.batch("[data-batch2]", {
-      start: "0% 70%",
-      end: "100% 70%",
-      // markers:true,
-      onEnter: function (batch) {
-        $(batch).addClass("on");
-      },
-      onLeaveBack: function (batch) {
-        $(batch).removeClass("on");
-      },
-    });
+      mobile = true;
+    }
   } else {
-    $("[data-batch2]").removeClass("on");
-    $(".header .group-mobile").removeClass("on");
-    $(".header .group-top .btn-menu").removeClass("on");
-    $("body").removeClass("hidden");
-    lenis.start();
+    if (mobile) {
+      mobileTriggers.forEach((trigger) => {
+        trigger.kill();
+        trigger.trigger.classList.remove('on');
+      });
+      groupMobile.classList.remove('on');
+      btnMenu.classList.remove('on');
+      document.body.classList.remove('hidden');
+      lenis.start();
+      mobile = false;
+    }
   }
 }
 
-ScrollTrigger.batch("[data-batch]", {
-  start: "0% 90%",
-  onEnter: function (batch) {
-    $(batch).addClass("on");
+// About trigger
+ScrollTrigger.create({
+  trigger: '.sc-about',
+  start: '0% 50%',
+  onEnter: () => {
+    gsap.to('.sc-about .group-title .title-area .line .word', {
+      opacity: 1,
+      duration: 1,
+      ease: 'power1.inOut',
+      stagger: {
+        each: 0.3,
+      },
+    });
+    initMotionPath();
+  },
+});
+
+// Study trigger
+ScrollTrigger.batch('[data-batch]', {
+  start: '0% 90%',
+  onEnter: function (batches) {
+    batches.forEach((batch) => {
+      batch.classList.add('on');
+    });
   },
 });
